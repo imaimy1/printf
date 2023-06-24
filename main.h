ef7b40d0b9b3dbd9616293b9008cd3ba5ab7fab0
imaimy1 <you@example.com>
2023-06-24 04:34:39 -0700

    add main.h

--- main.h
+++ main.h
@@ -1,6 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int print_char(va_list a, params_t *p);
+int print_string(va_list a, params_t *p);
+int print_percent(va_list a, params_t *p);
+int print_int(va_list a, params_t *p);
+
 int _printf(const char *format, ...);
 
 #endif /* MAIN_H */
