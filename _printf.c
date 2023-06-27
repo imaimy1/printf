4369805f9a3c28116f6b9305e4dda49b8da53104
imaimy1 <you@example.com>
2023-06-27 06:27:21 -0700

    add _printf.c

---
+++ _printf.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _printf - produces outputs according to a format.
+ * @format: format.
+ * return: somthing.
+ */
+
+int _printf(const char *format, ...)
+{
+      
+
+}
